58e0f71d4ce1d3bd96b9ec1a892e660041ec81f5
Orsu <Orsu>
2018-10-25 00:58:42 +0200

    i understood how i works. great

--- router.php
+++ router.php
@@ -5,10 +5,12 @@ class Router
 
     static public function parse($url, $request)
     {
+       
         $url = trim($url);
-
-        if ($url == "/")
-        {
+        
+        
+        if($url == "/project/"){
+            
             $request->controller = "tasks";
             $request->action = "index";
             $request->params = [];
@@ -16,7 +18,9 @@ class Router
         else
         {
             $explode_url = explode('/', $url);
+            print_r($explode_url);
             $explode_url = array_slice($explode_url, 2);
+            print_r($explode_url);
             $request->controller = $explode_url[0];
             $request->action = $explode_url[1];
             $request->params = array_slice($explode_url, 2);
