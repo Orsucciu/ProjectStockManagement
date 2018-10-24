80ef5f1bd3fe369a65390521d6fde2c6bd18d4a1
Orsu <Orsucciu@users.noreply.github.com>
2018-10-24 15:19:58 +0200

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -2,7 +2,27 @@
 
 class Controller{
     
+    var $vars = [];
+    var $layout = "default";
+    
+    function set($d)
+        {
+            $this->vars = array_merge($this->vars, $d);
+        }
+    
     function render($viewName){
         
+        extract($this->vars);
+        ob_start();
+        require("../view/")
+        $content_for_layout = ob_get_clean();
+            if ($this->layout == false)
+            {
+                $content_for_layout;
+            }
+            else
+            {
+                require(ROOT . "Views/Layouts/" . $this->layout . '.php');
+            }
     }
 }
