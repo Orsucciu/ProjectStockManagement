3c8892d720a0f1ec16cff4553c2890e2af53103d
Orsu <Orsu>
2018-10-25 15:27:26 +0200

    Created form
    Création du formulaire, côté vue

--- Models/Formulaire.php
+++ Models/Formulaire.php
@@ -4,10 +4,11 @@ class Formulaire extends Model {
     
     public function selectAll()
     {
-        $sql = "SELECT * FROM phone";
+        $sql = "SELECT * FROM materiel";
 
         $req = Database::getBdd()->prepare($sql);
+        $req->execute();
 
-        return $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
     }
 }
--- Views/Formulaire/index.php
+++ Views/Formulaire/index.php
@@ -1,7 +1,27 @@
 <h1>Form</h1>
 <div class="row col-md-12 centered">
-    HOLA
-    <?php 
-        print_r($form);
-    ?>
+    <form method="post" action="yet to decide lol">
+        Je soussigné : <input type="text" value=""><br>
+        Agissant en qualité de :<br>
+        <fieldset name="statut">
+            <input type="radio" name="statut" value="particulier" checked> Particulier<br>
+            <input type="radio" name="statut" value="etudiant"> Etudiant<br>
+        </fieldset>
+        Sollicite  le  prêt  des  différents  matériels  énumérés  ci-dessous et m’engage à  restituer la totalité de ces équipements dans un état de propreté correct. <br>
+        <table>
+            <tr>
+                <th>Désignation</th>
+                <th>Nom Type</th> 
+                <th>Quantité</th>
+                <th>Etat à la restitution</th>
+            </tr>
+            <!-- Ici, du code pour générer le tableau au fur et à mesure, et pour le remplir avec le sdonnées de la base-->
+            <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>50</td>
+            </tr>
+        </table>
+        <input type="submit">
+    </form>
 </div>
