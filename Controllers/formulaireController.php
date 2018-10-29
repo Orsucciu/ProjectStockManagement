e8e076a036b9af59c548e329def01e199edbf6bc
Orsu <Orsu>
2018-10-29 23:01:14 +0100

    added better library

--- Controllers/formulaireController.php
+++ Controllers/formulaireController.php
@@ -15,12 +15,16 @@ class formulaireController extends Controller
     
     function getPDF(){
         
-        require_once(ROOT . 'Libs/Fpdf/fpdf.php');
+        //require_once(ROOT . 'Libs/Fpdf/fpdf.php');
+        require_once(ROOT . 'Libs/fpdfm/fpdm.php');
         ob_start();
-        $pdf = new FPDF();
+        $pdf = new FPDF('P', 'mm', 'A4'); //document format declaration
         $pdf->AddPage();
-        $pdf->SetFont('Arial','B',16);
-        $pdf->Cell(40,10,'Hello World !');
+        $pdf->Image((ROOT . 'assets/logo.jpg'), 10, 10, 33, 25); //ajout du logo
+        $pdf->SetFont('Times', 'B',18);
+        $pdf->Cell(80,10, utf8_decode('Université de Corse'));
+        $pdf->SetFont('Times', 'B',18);
+        $pdf->Cell(40,40, utf8_decode('Faculté des Sciences et Techniques'));
         $pdf->Output();
         ob_end_flush();
     }
